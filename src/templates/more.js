a362cc87afea82f808c35cf01b5f763450c90957
cheadrian <che.adrian@yahoo.com>
2022-05-05 12:37:46 +0300

    Fix more; Fix some translations;

--- src/templates/more.js
+++ src/templates/more.js
@@ -1,6 +1,8 @@
 let html = `<div class="card-more selector">
-    <div class="card-more__title" data-i18n="more.title">
-        Еще
+    <div class="card-more__box">
+        <div class="card-more__title" data-i18n="more.title">
+            Еще
+        </div>
     </div>
 </div>`
 
